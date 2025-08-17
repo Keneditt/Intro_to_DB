c6080775427a90647e8215fd5e0523de9eee3508
Kutlwano Mhlongo <kutlwanomakhalemele@gmail.com>
2025-08-17 18:15:54 +0200

    added anotger code

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,12 @@
--- Switch to alx_book_store database and show full table description
-USE alx_book_store;
-SHOW CREATE TABLE books;
+-- Get full description of books table from information schema
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'books';
